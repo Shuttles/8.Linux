6109a653581caaf8e38ba715f978ab54a73a6d74
chenbin <2287441467@qq.com>
2020-05-26 21:18:15 +0800

    UDP

--- 3.network/1.Socket/2.UDP/client.c
+++ 3.network/1.Socket/2.UDP/client.c
@@ -2,55 +2,34 @@
 	> File Name: client.c
 	> Author: 
 	> Mail: 
-	> Created Time: 二  5/26 17:11:17 2020
+	> Created Time: 二  5/26 14:11:17 2020
  ************************************************************************/
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
-#define MAXBUF 256
-int main(int argc, char const *argv[])
-{
-	int s = 0;
-	int n = 0;
-	int reuse = 1;
-	int port = 1987;
-	struct sockaddr_in srv;
-	char buf[MAXBUF] = {0};
-	/*解析参数*/
-	if (argc != 2)
-	{
-		printf("Usage:%s ServerIP\n", argv[0]);
-		return -1;
-	}
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+int main() {
+    int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8888);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-	bzero(&srv, sizeof(srv));
-	srv.sin_family = PF_INET;
-	srv.sin_addr.s_addr = inet_addr(argv[1]);
-	srv.sin_port = htons(port);
-	/*创建 UDP 套节字*/
-	s = socket(AF_INET, SOCK_DGRAM, 0);
-	if(s<0){
-		perror("socket");
-		return -1;
-	}
-
-	while(1){
-		memset(buf, 0, MAXBUF);
-		/*读取用户输入到buf中*/
-		fgets(buf, MAXBUF, stdin);
-
-		/*通过套节字 s 向服务器发送数据*/
-		if ((n = sendto(s, buf, strlen(buf), 0, (struct sockaddr *) &srv, sizeof(struct sockaddr))) < 0)
-		{
-			perror("sendto");
-			return -1;
-		}else{
-			printf("send to %s(port=%d) len %d:%s\n", argv[1], port, n, buf);
-		}
-	}
+    char *buf= (char *)malloc(sizeof(char) * 1024);
+    while(1) {
+        memset(buf,0,sizeof(buf));
+        scanf("%s",buf);
+        sendto(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&addr,sizeof(addr));
+/*
+        socklen_t len=sizeof(addr);
+        recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&addr,&len);
+        printf("%s",buf);
+*/
+    }
+    close(sockfd);
+    return 0;
 }
-
--- 3.network/1.Socket/2.UDP/server.c
+++ 3.network/1.Socket/2.UDP/server.c
@@ -2,62 +2,44 @@
 	> File Name: server.c
 	> Author: 
 	> Mail: 
-	> Created Time: 二  5/26 17:11:49 2020
+	> Created Time: 二  5/26 10:11:49 2020
  ************************************************************************/
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<string.h>
+int main() {
+    int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8888);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    int ret = bind(sockfd,(struct sockaddr*)&addr,sizeof(addr));
 
-#define MAXBUF 256
-int main(int argc, char const *argv[])
-{
-	int s = 0;
-	int n = 0;
-	int reuse = 1;
-	int cli_len = sizeof(struct sockaddr);
-	int port = 1987;
-	char buf[MAXBUF] = {0};
-	struct sockaddr_in addr, cli;
+    if(ret < 0) {
+        perror("bind");
+        exit(1);
 
-	/*初始化本地监听端口信息*/
-	bzero(&addr, sizeof(addr));
-	addr.sin_family = PF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(port);
+    }
 
-	/*创建UDP套节字*/
-	s = socket(AF_INET, SOCK_DGRAM, 0);
-	if (s<0)
-	{
-		perror("socket");
-		return -1;
-	}
-
-	/*允许端口复用*/
-	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-
-	/*绑定指定端口*/
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-	{
-		perror("bind");
-		return -1;
-	}
-
-	while(1){
-		memset(buf, 0, MAXBUF);
-		/*从套节字s中读取数据*/
-		n = recvfrom(s, buf, MAXBUF, 0, (struct sockaddr *)&cli, &cli_len);
-		if(n<0){
-			perror("recvfrom");
-			return -1;
-		}else{
-			printf("receive msg from %s(port=%d) len %d: %s\n",inet_ntoa(cli.sin_addr), port, n, buf);
-		}
-	}
-
-	return 0;
+    struct sockaddr_in cli;
+    socklen_t len=sizeof(cli);
+    char * buf = (char *) malloc(sizeof(char) * 1024);
+    while(1)
+    {
+        memset(buf,0,sizeof(buf));
+        recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&cli,&len);
+        printf("recv =%s\n",buf);
+    /*
+        memset(buf,0,sizeof(buf));
+        buf ="have recved";
+        sendto(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&cli,len);
+    */
+    }
+    close(sockfd);
+    return 0;
 }
